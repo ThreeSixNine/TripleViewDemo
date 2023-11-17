cdf75c42a6e132fff488b687cd08fe79d5d8f5b4
Thon Ly <thonly@siliconwat.dev>
2023-11-16 20:11:08 -0800

    Initial Commit

---
+++ TripleViewDemo/TripleViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripleViewDemoApp.swift
+//  TripleViewDemo
+//
+//  Created by HeartBank® on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct TripleViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
